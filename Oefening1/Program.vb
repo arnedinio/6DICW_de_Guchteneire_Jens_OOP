ddf3406220a51861211072f3aaa3dda1172e9214
arnedinio <jensdg0604@gmail.com>
2025-09-18 12:27:42 +0200

    Zimbabwe

---
+++ Oefening1/Program.vb
@@ -0,0 +1,15 @@
+Imports System
+
+Module Program
+
+    Class Leeuw
+        Public intsnelheid As Integer
+        Public intleeftijd As Integer
+    End Class
+    Sub Main(args As String())
+        Dim objLeeuw As New Leeuw()
+        objLeeuw.intsnelheid = 24
+
+        Console.WriteLine("Hello World!")
+    End Sub
+End Module
