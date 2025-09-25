1b2988fe7359a133740dd124c3589d4f85f029f7
arnedinio <jensdg0604@gmail.com>
2025-09-25 12:28:55 +0200

    H5 oefening Auto verbeterd

---
+++ Oefening1.H5/Oefening1.H5/Program.vb
@@ -0,0 +1,12 @@
+Imports System
+Module Program
+
+    Class auto
+        Public dblBandenSpanning = 5.0
+    End Class
+
+    Sub Main(args As String())
+        Dim myAuto As New auto()
+        Console.WriteLine(myAuto.dblBandenSpanning)
+    End Sub
+End Module
